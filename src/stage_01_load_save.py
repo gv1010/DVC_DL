5edbcd64d1a5bfcc43ff5851cfdc29aaf979a24d
gv281093@gmail.com <gv281093@gmail.com>
2022-01-08 22:44:44 +0530

    added code to stage_01_load_save

--- src/stage_01_load_save.py
+++ src/stage_01_load_save.py
@@ -1,25 +1,29 @@
 import os.path
-
+import shutil
 from src.utils.all_utils import read_yaml, create_directory
 import argparse
+from tqdm import tqdm
 import pandas as pd
+
+
+def copy_file(source_download_dir, local_data_dir):
+    list_of_files = os.listdir(source_download_dir)
+    N = len(list_of_files)
+    for file in tqdm(list_of_files, total=N, desc = f"copying file{source_download_dir} to {local_data_dir}", color="red"):
+        src = os.path.join(source_download_dir, file)
+        dest = os.path.join(local_data_dir, file)
+        shutil.copy(src, dest)
 
 def get_data(config_path):
     config = read_yaml(config_path)
+    source_download_dirs = config["source_data_paths"]
+    local_data_dirs = config["local_data_dirs"]
 
-    remote_data_path = config["data_source"]
-    df = pd.read_csv(remote_data_path, sep = ";")
-    # save dataset in the local directory
-    # create path to directory: artifacts/raw_local_dir/data.csv
-    artifacts_dir = config["artifacts"]["artifacts_dir"]
-    raw_local_dir = config["artifacts"]["raw_local_dir"]
-    raw_local_file = config["artifacts"]["raw_local_file"]
+    for source_download_dir, local_data_dir in tqdm(zip(source_download_dirs, local_data_dirs), total=2, desc= "list folders", color="green"):
+        create_directory(source_download_dir)
+        create_directory(local_data_dir)
+        copy_file(source_download_dir, local_data_dir)
 
-    raw_local_dir_path = os.path.join(artifacts_dir, raw_local_dir)
-    create_directory(dirs = [raw_local_dir_path])
-    raw_local_file_path = os.path.join(raw_local_dir_path, raw_local_file)
-
-    df.to_csv(raw_local_file_path, sep=",", index = False)
 
 
 
