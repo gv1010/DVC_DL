7173e8afbc8a702f6461fb1a7e15769fe874d64b
gv281093@gmail.com <gv281093@gmail.com>
2022-01-09 12:36:36 +0530

    added logging

--- src/stage_01_load_save.py
+++ src/stage_01_load_save.py
@@ -4,6 +4,13 @@ from src.utils.all_utils import read_yaml, create_directory
 import argparse
 from tqdm import tqdm
 import pandas as pd
+import logging
+
+logging_str = "[%(asctime)s: %(levelname)s: %(module)s]: %(message)s"
+log_dir = "logs"
+os.makedirs(log_dir, exist_ok=True)
+logging.basicConfig(filename=os.path.join(log_dir, 'running_logs.log'), level=logging.INFO, format=logging_str,
+                    filemode="a")
 
 
 def copy_file(source_download_dir, local_data_dir):
@@ -31,4 +38,11 @@ if __name__ == "__main__":
     args = argparse.ArgumentParser()
     args.add_argument("--config", "-c", default = "config/config.yaml")
     parsed_args = args.parse_args()
-    get_data(config_path = parsed_args.config)
+
+    try:
+        logging.info(">>>>> stage one started")
+        get_data(config_path=parsed_args.config)
+        logging.info("stage one completed! all the data are saved in local >>>>>\n")
+    except Exception as e:
+        logging.exception(e)
+        raise e
